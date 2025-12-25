e8a5b7007571d3fc245d51223661c7faf9968aab
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2025-12-25 13:21:49 +0300

    feat: solve module 4

--- exercises/04_traits/09_from/src/lib.rs
+++ exercises/04_traits/09_from/src/lib.rs
@@ -1,10 +1,16 @@
 // TODO: Implement the `From` trait for the `WrappingU32` type to make `example` compile.
 
-pub struct WrappingU32 {
-    value: u32,
+pub struct WrappingU32<T> {
+    value: T,
+}
+
+impl<T> From<T> for WrappingU32<T> {
+    fn from(value: T) -> Self {
+        Self { value }
+    }
 }
 
 fn example() {
-    let wrapping: WrappingU32 = 42.into();
-    let wrapping = WrappingU32::from(42);
+    let wrapping: WrappingU32<u32> = 42.into();
+    let wrapping: WrappingU32<i32> = WrappingU32::from(42);
 }
